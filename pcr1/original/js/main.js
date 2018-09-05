72784e90852ead1d686d114f72894a362aab3b9e
marius-mac <marius.mackevic@gmail.com>
2018-09-05 11:13:32 +0300

    c2

---
+++ pcr1/original/js/main.js
@@ -0,0 +1,21 @@
+// =================Patikrinimas ar veikia==========================
+console.log(" ");
+console.log(" ");
+
+// =================paleidziamas js tik uzkrovus html (js pradzia)==
+$ (document).ready(function(){
+
+// =================Patikrinimas ar veikia js========================
+
+document.querySelector("h2").innerHTML += " ";
+document.querySelector("h2").innerHTML += "<br>" + " ";
+// =================Patikrinimas ar veikia jquery====================
+$ ("h2").css("color", "red");
+
+
+// =================2-jQ-efects=====================================
+
+
+
+// ================js pabaiga=======================================
+});
